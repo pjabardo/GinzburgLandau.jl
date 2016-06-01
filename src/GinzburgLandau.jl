3de8a5baca9345ed29ec4c6e35c423babaec612d
Paulo Jabardo <pjabardo@yahoo.com.br>
2016-06-01 19:11:28 -0300

    Cleaned up Euler code. Working on Adams-Bashfoth
    For now I will try to use an explicit Adams-Bashfort solver.

--- src/GinzburgLandau.jl
+++ src/GinzburgLandau.jl
@@ -5,12 +5,19 @@ module GinzburgLandau
 dct_nodes(N, L) = (0:N-1)*L/N + L/(2N)
 
 
-AdamsMoulton = [1     0      0     0    0    0;
-                1     1      0     0    0    0;
-                5     8     -1     0    0    0;
-                9     19    -5     1    0    0;
-                251   646  -264  106  -19    0;
-                475   1427 -798  482 -173   27]
+AdamsMoulton = [1          0          0         0         0          0;
+                1/2        1/2        0         0         0          0;
+                5/12       8/12      -1/12      0         0          0;
+                9/24       19/24     -5/24      1/24      0          0;
+                251/720    646/720   -264/720   106/720  -19/720     0;
+                475/1440   1427/1440 -798/1440  482/1440 -173/1440   27/1440]
+
+AdamsBashforth=[1          0          0          0        0          0;
+                3/2        -1/2       0          0        0          0;
+                23/12      -16/12     5/12       0        0          0;
+                55/24      -59/24     37/24     -9/24     0          0;  
+                1901/720   -2774/720  2616/720  -1274/720  251/720   0;
+                4277/1440  -7923/1440 9982/1440 -7298/1440 2877/1440 -475/1440]
 
 type GLECoeffs
     σ::Float64
@@ -40,7 +47,6 @@ type GLESolver1
     Δt::Float64
     δ::Vector{Complex{Float64}}
     g::Vector{Complex{Float64}}
-    a1::Vector{Complex{Float64}}
 end
 
 function GLESolver1(L, N, Δt, k::GLECoeffs)
@@ -53,19 +59,12 @@ function GLESolver1(L, N, Δt, k::GLECoeffs)
     end
 
     g = zeros(Complex{Float64}, N)
-    a1 = zeros(Complex{Float64}, N)
-    GLESolver1(L, N, Δt, δ, g, a1)
+    GLESolver1(L, N, Δt, δ, g)
 end
 
 nmodes(slv::GLESolver1) = slv.N
 nodes(slv::GLESolver1) = dct_nodes(slv.N, slv.L)
 
-
-function gle_init!(slv::GLESolver1, a::Vector{Complex{Float64}})
-
-    copy!(slv.a1, a)
-    return nothing
-end
 
 function gle_step!(slv::GLESolver1, k::GLECoeffs, a₀::AbstractVector{Complex{Float64}},
                    a₁::AbstractVector{Complex{Float64}})
@@ -104,7 +103,14 @@ end
 
 
 
-    
+type GLESolverAdams
+    L::Float64
+    N::Int
+    Δt::Float64
+    δ::Vector{Complex{Float64}}
+    g::Vector{Complex{Float64}}
+    a1::Vector{Complex{Float64}}
+end    
 
                     
 end # module
